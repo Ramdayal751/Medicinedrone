b65a33132d5659b1c46af2c40ecb5fa2ce92a8fb
Ramdayal751 <108316937+Ramdayal751@users.noreply.github.com>
2023-05-23 18:13:06 +0530

    Update script.js

--- login/script.js
+++ login/script.js
@@ -4,19 +4,19 @@ function login() {
    
   if (username === "ram" && password === "987456321@") {
     alert("Login successful!");
-    window.open('http://192.168.249.39/');
+    window.open('http://192.168.66.39/');
   }
   else if (username === "dhiraj" && password === "06061999") {
     alert("Login successful!");
-    window.open('http://192.168.249.39/');
+    window.open('http://192.168.66.39/');
   }
   else if (username === "abc" && password === "12345@") {
     alert("Login successful!");
-    window.open('http://192.168.249.39/');
+    window.open('http://192.168.66.39/');
   }
   else if (username === "bkp" && password === "bh12@#ra") {
     alert("Login successful!");
-    window.open('http://192.168.249.39/');
+    window.open('http://192.168.66.39/');
   }
   else {
     alert("Invalid username or password");
